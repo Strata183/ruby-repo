3c31bd7955d2f2eb7b5b96818bc4337e32b7e410
Derek Smith <derek.a.smith.76@gmail.com>
2026-09-22 00:46:01 +0000

    categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_19_203225) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_22_003617) do
   create_table "articles", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
     t.string "status"
     t.string "title"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
     t.datetime "updated_at", null: false
   end
 
